fac7937ef2fcfe74eb526dd136c05114853b1632
Srul1k <kuch1kukan@keemail.me>
2021-06-06 16:14:19 +0300

    Initialize project

---
+++ src/Northwind/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Northwind.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public Supplier Supplier { get; set; }
+        public Category Category { get; set; }
+        public string QuantityPerUnit { get; set; }
+        [Column(TypeName = "money")]
+        public decimal UnitPrice { get; set; }
+        public short UnitsInStock { get; set; }
+        public short UnitsOnOrder { get; set; }
+        public short ReorderLevel { get; set; }
+        public bool Discontinued { get; set; }
+    }
+}
