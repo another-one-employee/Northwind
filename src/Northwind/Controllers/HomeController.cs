1fefbc5814735e0705ef76468d54e65f365394a8
Srul1k <kuch1kukan@keemail.me>
2021-06-14 17:28:01 +0300

    Add ProductsController
    * Moved methods from HomeController
    
    * Also moved views

--- src/Northwind/Controllers/HomeController.cs
+++ src/Northwind/Controllers/HomeController.cs
@@ -1,15 +1,11 @@
 ﻿using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Northwind.Data.Models;
-using Northwind.Models;
 using Northwind.Models.HomeViewModels;
 using System;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Northwind.Controllers
@@ -17,18 +13,16 @@ namespace Northwind.Controllers
     public class HomeController : Controller
     {
         private readonly NorthwindDataContext _db;
-        private readonly IConfiguration _configuration;
         private readonly ILogger<ErrorViewModel> _loggerOfErrors;
 
         public HomeController(
             NorthwindDataContext db,
-            IConfiguration configuration,
             ILogger<ErrorViewModel> loggerOfErrors)
         {
             _db = db;
-            _configuration = configuration;
             _loggerOfErrors = loggerOfErrors;
         }
+
         public IActionResult Index()
         {
             return View();
@@ -38,78 +32,6 @@ namespace Northwind.Controllers
         {
             var categories = await _db.Categories.ToListAsync();
             return View(categories);
-        }
-
-        public async Task<IActionResult> Products()
-        {
-            int amount = _configuration.GetValue<int>("MaximumAmmountOfProducts");
-            var products = _db.Products
-                .Include(s => s.Supplier)
-                .Include(c => c.Category);
-
-            if (amount == 0)
-            {
-                return View(await products.ToListAsync());
-            }
-
-            return View(await products.Take(amount).ToListAsync());
-        }
-
-        [HttpGet]
-        public IActionResult Create()
-        {
-            ViewBag.Suppliers = new SelectList(_db.Suppliers, "SupplierID", "CompanyName");
-            ViewBag.Categories = new SelectList(_db.Categories, "CategoryID", "CategoryName");
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Create(Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                _db.Add(product);
-                await _db.SaveChangesAsync();
-
-                return RedirectToAction("Products");
-            }
-            else
-            {
-                return View(product);
-            }
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id != null)
-            {
-                Product product = await _db.Products.FirstOrDefaultAsync(p => p.ProductID == id);
-                if (product != null)
-                {
-                    ViewBag.Suppliers = new SelectList(_db.Suppliers, "SupplierID", "CompanyName");
-                    ViewBag.Categories = new SelectList(_db.Categories, "CategoryID", "CategoryName");
-                    return View(product);
-                }
-            }
-
-            return NotFound();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Edit(Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                _db.Products.Update(product);
-                await _db.SaveChangesAsync();
-
-                return RedirectToAction("Products");
-            }
-            else
-            {
-                return View(product);
-            }
         }
 
         public IActionResult Error()
---
+++ src/Northwind/Controllers/ProductsController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Northwind.Data.Models;
+using Northwind.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northwind.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly NorthwindDataContext _db;
+        private readonly int _maximumAmmountOfProducts;
+
+        public ProductsController(
+            NorthwindDataContext db,
+            IConfiguration configuration)
+        {
+            _db = db;
+            _maximumAmmountOfProducts =
+                configuration.GetValue<int>("MaximumAmmountOfProducts");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = _db.Products
+                .Include(s => s.Supplier)
+                .Include(c => c.Category);
+
+            if (_maximumAmmountOfProducts == 0)
+            {
+                return View(await products.ToListAsync());
+            }
+
+            return View(await products.Take(_maximumAmmountOfProducts).ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Suppliers = new SelectList(_db.Suppliers, "SupplierID", "CompanyName");
+            ViewBag.Categories = new SelectList(_db.Categories, "CategoryID", "CategoryName");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(product);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                Product product = await _db.Products.FirstOrDefaultAsync(p => p.ProductID == id);
+                if (product != null)
+                {
+                    ViewBag.Suppliers = new SelectList(_db.Suppliers, "SupplierID", "CompanyName");
+                    ViewBag.Categories = new SelectList(_db.Categories, "CategoryID", "CategoryName");
+                    return View(product);
+                }
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Update(product);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+    }
+}
