a8fef484afa5b6e7ba515b515bb432c56c76f631
Srul1k <kuch1kukan@keemail.me>
2021-06-24 11:27:41 +0300

    Add CategoryController
    * Moved method from HomeController
    
    * Also moved views

---
+++ src/Northwind/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Northwind.Data.Models;
+using System.Threading.Tasks;
+
+namespace Northwind.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly NorthwindDataContext _db;
+        public CategoryController(NorthwindDataContext db)
+        {
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _db.Categories.ToListAsync();
+            return View(categories);
+        }
+    }
+}
