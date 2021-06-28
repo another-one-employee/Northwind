804dc9a6c9086004b4f672141f764e5334a84a5f
Srul1k <kuch1kukan@keemail.me>
2021-06-28 13:07:41 +0300

    Add methods for displaying/editing Category images

--- src/Northwind/Models/Category.cs
+++ src/Northwind/Models/Category.cs
@@ -5,5 +5,6 @@
         public int CategoryID { get; set; }
         public string CategoryName { get; set; }
         public string Description { get; set; }
+        public byte[] Picture { get; set; }
     }
 }
