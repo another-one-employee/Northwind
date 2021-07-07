6e9b0bb2b0bd4b5977de3abf67fb17d92fc60775
Srul1k <kuch1kukan@keemail.me>
2021-07-07 16:58:58 +0300

    Update hamburger menu

--- src/Northwind.Web/wwwroot/js/site.js
+++ src/Northwind.Web/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "165px";
+    document.getElementById("sidebarInvoker").style.marginLeft = "165px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("sidebarInvoker").style.marginLeft = "0";
+}
