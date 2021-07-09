8d58d0c8747e16e1ac542af488cd561b741a6a8f
Srul1k <kuch1kukan@keemail.me>
2021-07-09 15:17:56 +0300

    Add Breadcrumbs
    * Changed Navbar and Hamburger menu style

---
+++ src/Northwind.Web/Components/Breadcrumb.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Northwind.Web.Components
+{
+    public class Breadcrumb : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var link = GetBreadcrumbItem();
+            return View(link);
+        }
+
+        public BreadcrumbItem GetBreadcrumbItem()
+        {
+            var controller = ViewContext.RouteData.Values["controller"].ToString();
+            var action = ViewContext.RouteData.Values["action"].ToString();
+
+            return new BreadcrumbItem() { Action = action, Controller = controller };
+        }
+    }
+}
---
+++ src/Northwind.Web/Components/BreadcrumbItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Northwind.Web.Components
+{
+    public class BreadcrumbItem
+    {
+        public string Controller { get; set; }
+        public string Action { get; set; }
+    }
+}
