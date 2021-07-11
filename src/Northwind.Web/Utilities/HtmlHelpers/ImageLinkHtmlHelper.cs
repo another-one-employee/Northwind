cbd8e9f2c695045e14a2c23002a8e870063200c3
Srul1k <kuch1kukan@keemail.me>
2021-07-11 16:53:55 +0300

    Add Helpers

---
+++ src/Northwind.Web/Utilities/HtmlHelpers/ImageLinkHtmlHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using System.Text.Encodings.Web;
+
+namespace Northwind.Web.Utilities.HtmlHelpers
+{
+    public static class ImageLinkHtmlHelper
+    {
+        public static HtmlString ImageLink(this IHtmlHelper html, int imageId, string linkText)
+        {
+            using var writer = new System.IO.StringWriter();
+            var url = new UrlHelper(html.ViewContext);
+
+            TagBuilder a = new("a");
+            a.InnerHtml.Append(linkText);
+            a.MergeAttribute("href", $"images/{imageId}");
+
+            a.WriteTo(writer, HtmlEncoder.Default);
+            return new HtmlString(writer.ToString());
+        }
+    }
+}
