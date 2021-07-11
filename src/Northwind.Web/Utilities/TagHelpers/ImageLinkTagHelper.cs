ac5bfe60085a894acd2db725dbb70c640c65482c
Srul1k <kuch1kukan@keemail.me>
2021-07-11 16:53:55 +0300

    Add Helpers

---
+++ src/Northwind.Web/Utilities/TagHelpers/ImageLinkTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Northwind.Web.Utilities.TagHelpers
+{
+    public class ImageLinkTagHelper : TagHelper
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.SetAttribute("href", $"images/{Id}");
+            output.Content.SetContent(Text);
+        }
+    }
+}
