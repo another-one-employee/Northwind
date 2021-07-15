0308af9137c813474a7214135169685e79e53e74
Srul1k <kuch1kukan@keemail.me>
2021-07-15 12:42:15 +0300

    Add CategoryService

---
+++ src/Northwind.Core/Services/CategoryService.cs
@@ -0,0 +1,40 @@
+﻿using Northwind.Core.Exceptions;
+using Northwind.Core.Interfaces;
+using Northwind.Core.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Northwind.Core.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IAsyncRepository<CategoryDTO> _categoryRepository;
+
+        public CategoryService(IAsyncRepository<CategoryDTO> categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<IEnumerable<CategoryDTO>> GetAllAsync()
+        {
+            return await _categoryRepository.FindAllAsync();
+        }
+
+        public async Task<CategoryDTO> GetByIdAsync(int id)
+        {
+            return await _categoryRepository.FindAsync(id) ?? throw new NotFoundException(nameof(CategoryDTO), id);
+        }
+
+        public async Task<byte[]> GetPictureByIdAsync(int id)
+        {
+            var category = await GetByIdAsync(id);
+
+            return category.Picture;
+        }
+
+        public async Task UpdateAsync(CategoryDTO category)
+        {
+            await _categoryRepository.Update(category);
+        }
+    }
+}
