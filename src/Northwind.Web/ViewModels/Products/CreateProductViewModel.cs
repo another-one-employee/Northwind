090e88320dd3fd674f83aa12d42ee278c5958c7f
Srul1k <kuch1kukan@keemail.me>
2021-07-20 12:58:54 +0300

    Update client-side validation

--- src/Northwind.Web/ViewModels/Products/CreateProductViewModel.cs
+++ src/Northwind.Web/ViewModels/Products/CreateProductViewModel.cs
@@ -1,20 +1,31 @@
 ﻿using Northwind.Core.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Northwind.Web.ViewModels.Products
 {
     public class CreateProductViewModel
     {
+        [Required]
+        [StringLength(50)]
         public string ProductName { get; set; }
 
+        [StringLength(20)]
         public string QuantityPerUnit { get; set; }
 
+        [DataType(DataType.Currency)]
+        [Range(0, 922337203685477.5807)]
         public decimal UnitPrice { get; set; }
 
+        [Range(0, short.MaxValue, ErrorMessage = "The value must be positive")]
         public short UnitsInStock { get; set; }
+
+        [Range(0, short.MaxValue, ErrorMessage = "The value must be positive")]
         public short UnitsOnOrder { get; set; }
 
+        [Range(0, short.MaxValue, ErrorMessage = "The value must be positive")]
         public short ReorderLevel { get; set; }
 
+        [Required]
         public bool Discontinued { get; set; }
 
         public int SupplierID { get; set; }
--- src/Northwind.Web/ViewModels/Products/EditProductViewModel.cs
+++ src/Northwind.Web/ViewModels/Products/EditProductViewModel.cs
@@ -1,21 +1,34 @@
 ﻿using Northwind.Core.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Northwind.Web.ViewModels.Products
 {
     public class EditProductViewModel
     {
+        [Key]
         public int ProductID { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string ProductName { get; set; }
 
+        [StringLength(20)]
         public string QuantityPerUnit { get; set; }
 
+        [DataType(DataType.Currency)]
+        [Range(0, 922337203685477.5807)]
         public decimal UnitPrice { get; set; }
 
+        [Range(0, short.MaxValue, ErrorMessage = "The value must be positive")]
         public short UnitsInStock { get; set; }
+
+        [Range(0, short.MaxValue, ErrorMessage = "The value must be positive")]
         public short UnitsOnOrder { get; set; }
 
+        [Range(0, short.MaxValue, ErrorMessage = "The value must be positive")]
         public short ReorderLevel { get; set; }
 
+        [Required]
         public bool Discontinued { get; set; }
 
         public int SupplierID { get; set; }
