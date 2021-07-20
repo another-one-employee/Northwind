516e959df6635225dbe6b97e178023dc0ad20238
Srul1k <kuch1kukan@keemail.me>
2021-07-20 12:58:54 +0300

    Update client-side validation

--- src/Northwind.Web/ViewModels/Categories/EditImageViewModel.cs
+++ src/Northwind.Web/ViewModels/Categories/EditImageViewModel.cs
@@ -1,9 +1,14 @@
-﻿namespace Northwind.Web.ViewModels.Categories
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Northwind.Web.ViewModels.Categories
 {
     public class EditImageViewModel
     {
+        [Required]
         public int CategoryID { get; set; }
+        [Required]
         public string CategoryName { get; set; }
+        [Required]
         public byte[] Picture { get; set; }
     }
 }
