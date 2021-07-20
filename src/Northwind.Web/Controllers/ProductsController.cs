af005256c9e70b0c94d05e84b094b332a3e20677
Srul1k <kuch1kukan@keemail.me>
2021-07-20 11:00:58 +0300

    Add Products ViewModels
    * Fix tests

--- src/Northwind.Web/Controllers/ProductsController.cs
+++ src/Northwind.Web/Controllers/ProductsController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Configuration;
 using Northwind.Core.Interfaces;
 using Northwind.Core.Models;
 using Northwind.Web.Utilities.Filters;
+using Northwind.Web.ViewModels.Products;
 using System;
 using System.Threading.Tasks;
 
@@ -15,6 +17,7 @@ namespace Northwind.Web.Controllers
         private readonly IProductService _productService;
         private readonly IAsyncRepository<SupplierDTO> _dbSupplier;
         private readonly IAsyncRepository<CategoryDTO> _dbCategory;
+        private readonly IMapper _mapper;
         private readonly Lazy<int> _lazyMaxAmountOfProducts;
         protected int MaxAmountOfProducts => _lazyMaxAmountOfProducts.Value;
 
@@ -22,11 +25,13 @@ namespace Northwind.Web.Controllers
             IProductService productService,
             IAsyncRepository<SupplierDTO> dbSupplier,
             IAsyncRepository<CategoryDTO> dbCategory,
-            IConfiguration configuration)
+            IConfiguration configuration,
+            IMapper mapper)
         {
             _productService = productService;
             _dbSupplier = dbSupplier;
             _dbCategory = dbCategory;
+            _mapper = mapper;
             _lazyMaxAmountOfProducts = new Lazy<int>(
                 configuration.GetValue<int>(nameof(MaxAmountOfProducts)));
         }
@@ -44,11 +49,11 @@ namespace Northwind.Web.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(ProductDTO product)
+        public async Task<IActionResult> Create(CreateProductViewModel product)
         {
             if (ModelState.IsValid)
             {
-                await _productService.CreateAsync(product);
+                await _productService.CreateAsync(_mapper.Map<ProductDTO>(product));
 
                 return RedirectToAction("Index");
             }
@@ -63,15 +68,15 @@ namespace Northwind.Web.Controllers
         public async Task<IActionResult> Edit(int id)
         {
             await PopulateProductsDropDownLists();
-            return View(await _productService.GetByIdAsync(id));
+            return View(_mapper.Map<EditProductViewModel>(await _productService.GetByIdAsync(id)));
         }
 
         [HttpPost]
-        public async Task<IActionResult> Edit(ProductDTO product)
+        public async Task<IActionResult> Edit(EditProductViewModel product)
         {
             if (ModelState.IsValid)
             {
-                await _productService.UpdateAsync(product);
+                await _productService.UpdateAsync(_mapper.Map<ProductDTO>(product));
                 return RedirectToAction("Index");
             }
             else
