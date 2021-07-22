bcc2f0f6890f853a4601a3a926589a8e2f5374d2
Srul1k <kuch1kukan@keemail.me>
2021-07-22 17:22:14 +0300

    Add Api Controllers

---
+++ src/Northwind.Web/Controllers/Api/CategoriesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Core.Interfaces;
+using System.Threading.Tasks;
+
+namespace Northwind.Web.Controllers.Api
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                return Ok(await _categoryService.GetAllAsync());
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Failed to get categories");
+            }
+        }
+    }
+}
