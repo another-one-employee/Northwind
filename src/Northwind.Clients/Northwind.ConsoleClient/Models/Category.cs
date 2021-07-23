333d1c12b32ec5a2511d45bc89b8b636a9205b18
Srul1k <kuch1kukan@keemail.me>
2021-07-23 11:04:33 +0300

    Add ConsoleClient

---
+++ src/Northwind.Clients/Northwind.ConsoleClient/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace Northwind.ConsoleClient.Models
+{
+    public class Category
+    {
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ src/Northwind.Clients/Northwind.ConsoleClient/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace Northwind.ConsoleClient.Models
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+
+        public string QuantityPerUnit { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public Supplier Supplier { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
