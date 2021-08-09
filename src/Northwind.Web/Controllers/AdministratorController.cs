150d7820850496e4d6100b84d5e7fc8cf478edf8
Srul1k <kuch1kukan@keemail.me>
2021-08-09 22:42:31 +0300

    Add authorization

---
+++ src/Northwind.Web/Controllers/AdministratorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Northwind.Web.Controllers
+{
+    [Authorize(Roles="admin")]
+    public class AdministratorController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+ 
+        public AdministratorController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+ 
+        public IActionResult Index() => View(_userManager.Users.ToList());
+    }
+}
