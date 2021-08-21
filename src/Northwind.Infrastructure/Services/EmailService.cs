ead55c5863d7835f04a6f6dd7960b52643af3c9f
Srul1k <72085557+Srul1k@users.noreply.github.com>
2021-08-21 21:59:43 +0300

    Update EmailService

--- src/Northwind.Infrastructure/Services/EmailService.cs
+++ src/Northwind.Infrastructure/Services/EmailService.cs
@@ -1,4 +1,5 @@
 ﻿using MailKit.Net.Smtp;
+using Microsoft.Extensions.Configuration;
 using MimeKit;
 using Northwind.Application.Interfaces;
 using System.Threading.Tasks;
@@ -7,12 +8,22 @@ namespace Northwind.Infrastructure.Services
 {
     public class EmailService : IEmailService
     {
-        // TODO: set email and pass (2021/08/09 10:08 PM)
+        private readonly string _email;
+        private readonly string _password;
+
+        public EmailService(IConfiguration configuration)
+        {
+            var emailServiceSettings = configuration.GetSection(nameof(EmailService));
+
+            _email = emailServiceSettings.GetSection("Email").Value;
+            _password = emailServiceSettings.GetSection("Password").Value;
+        }
+
         public async Task SendEmailAsync(string email, string subject, string message)
         {
             var emailMessage = new MimeMessage();
  
-            emailMessage.From.Add(new MailboxAddress("Northwind administration", "set email"));
+            emailMessage.From.Add(new MailboxAddress("Northwind administration", _email));
             emailMessage.To.Add(new MailboxAddress("", email));
             emailMessage.Subject = subject;
             emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
@@ -22,7 +33,7 @@ namespace Northwind.Infrastructure.Services
 
             using var client = new SmtpClient();
             await client.ConnectAsync("smtp.gmail.com", 465, true);
-            await client.AuthenticateAsync("set email", "set pass");
+            await client.AuthenticateAsync(_email, _password);
 
             await client.SendAsync(emailMessage);
 
