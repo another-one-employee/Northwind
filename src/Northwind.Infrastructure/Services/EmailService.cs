8a0e5c6553c501453b51b4ca955b2af315d58686
Srul1k <72085557+Srul1k@users.noreply.github.com>
2021-08-30 11:25:00 +0300

    Update EmailService configuration

--- src/Northwind.Infrastructure/Services/EmailService.cs
+++ src/Northwind.Infrastructure/Services/EmailService.cs
@@ -8,6 +8,13 @@ namespace Northwind.Infrastructure.Services
 {
     public class EmailService : IEmailService
     {
+        private const string EmailSection = "Email";
+        private const string PasswordSection = "Password";
+        private const string MailBoxAddressName = "Northwind administration";
+        private const string MailServiceHost = "smtp.gmail.com";
+        private const int MailServicePort = 456;
+
+
         private readonly string _email;
         private readonly string _password;
 
@@ -15,15 +22,15 @@ namespace Northwind.Infrastructure.Services
         {
             var emailServiceSettings = configuration.GetSection(nameof(EmailService));
 
-            _email = emailServiceSettings.GetSection("Email").Value;
-            _password = emailServiceSettings.GetSection("Password").Value;
+            _email = emailServiceSettings.GetSection(EmailSection).Value;
+            _password = emailServiceSettings.GetSection(PasswordSection).Value;
         }
 
         public async Task SendEmailAsync(string email, string subject, string message)
         {
             var emailMessage = new MimeMessage();
  
-            emailMessage.From.Add(new MailboxAddress("Northwind administration", _email));
+            emailMessage.From.Add(new MailboxAddress(MailBoxAddressName, _email));
             emailMessage.To.Add(new MailboxAddress("", email));
             emailMessage.Subject = subject;
             emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
@@ -32,7 +39,7 @@ namespace Northwind.Infrastructure.Services
             };
 
             using var client = new SmtpClient();
-            await client.ConnectAsync("smtp.gmail.com", 465, true);
+            await client.ConnectAsync(MailServiceHost, MailServicePort, true);
             await client.AuthenticateAsync(_email, _password);
 
             await client.SendAsync(emailMessage);
