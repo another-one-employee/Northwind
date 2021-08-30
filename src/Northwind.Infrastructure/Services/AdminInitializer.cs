9bbc92c2889f074360951e727286b65b8630746e
Srul1k <72085557+Srul1k@users.noreply.github.com>
2021-08-30 11:20:10 +0300

    Update AdminInitializer configuration

--- src/Northwind.Infrastructure/Services/AdminInitializer.cs
+++ src/Northwind.Infrastructure/Services/AdminInitializer.cs
@@ -2,11 +2,15 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Northwind.Infrastructure.Annotations;
 
 namespace Northwind.Infrastructure.Services
 {
     public static class AdminInitializer
     {
+        private static string emailSection = "Email";
+        private static string passwordSection = "Password";
+
         public static async void InitializeAdminAsync(this IApplicationBuilder app, IConfiguration configuration)
         {
             using var scope = app.ApplicationServices.CreateScope();
@@ -14,12 +18,12 @@ namespace Northwind.Infrastructure.Services
             var roleManager = scope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
             var adminSetting = configuration.GetSection(nameof(AdminInitializer));
 
-            var email = adminSetting.GetSection("Email").Value;
-            var password = adminSetting.GetSection("Password").Value;
+            var email = adminSetting.GetSection(emailSection).Value;
+            var password = adminSetting.GetSection(passwordSection).Value;
 
-            if (await roleManager.FindByNameAsync("admin") == null)
+            if (await roleManager.FindByNameAsync(nameof(Roles.admin)) == null)
             {
-                await roleManager.CreateAsync(new IdentityRole("admin"));
+                await roleManager.CreateAsync(new IdentityRole(nameof(Roles.admin)));
             }
 
             if (await userManager.FindByNameAsync(email) == null)
@@ -28,7 +32,7 @@ namespace Northwind.Infrastructure.Services
                 IdentityResult result = await userManager.CreateAsync(admin, password);
                 if (result.Succeeded)
                 {
-                    await userManager.AddToRoleAsync(admin, "admin");
+                    await userManager.AddToRoleAsync(admin, nameof(Roles.admin));
                 }
             }
         }
