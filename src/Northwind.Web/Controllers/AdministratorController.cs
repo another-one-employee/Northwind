64c9e686ba7a0d0b0e4715cbbcd337da08bd08e2
Srul1k <72085557+Srul1k@users.noreply.github.com>
2021-08-30 11:08:45 +0300

    Add Annotations for Roles

--- src/Northwind.Web/Controllers/AdministratorController.cs
+++ src/Northwind.Web/Controllers/AdministratorController.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Northwind.Infrastructure.Annotations;
 using System.Linq;
 
 namespace Northwind.Web.Controllers
 {
-    [Authorize(Roles="admin")]
+    [Authorize(Roles=nameof(Roles.admin))]
     public class AdministratorController : Controller
     {
         private readonly UserManager<IdentityUser> _userManager;
