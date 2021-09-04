09d336e6342e0733dc175890716d982e9e8731f7
Srul1k <72085557+Srul1k@users.noreply.github.com>
2021-09-04 10:19:18 +0300

    Add AdministratorControllerTests

---
+++ tests/Northwind.Web.UnitTests/Controllers/AdministratorControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Northwind.Web.Controllers;
+using NUnit.Framework;
+
+namespace Northwind.Web.UnitTests.Controllers
+{
+    public class AdministratorControllerTests
+    {
+        [Test]
+        public void Index_ReturnsViewResult()
+        {
+            // Arrange
+            var userStoreMock = new Mock<IUserStore<IdentityUser>>();
+            var userManager = new Mock<UserManager<IdentityUser>>(
+                userStoreMock.Object, null, null, null, null, null, null, null, null);
+
+            var controller = new AdministratorController(userManager.Object);
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.IsInstanceOf<ViewResult>(result);
+        }
+    }
+}
