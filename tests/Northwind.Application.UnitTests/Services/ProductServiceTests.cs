8beed6cb710e8c3b434c49cc8200de0eb87d7c65
Srul1k <72085557+Srul1k@users.noreply.github.com>
2021-09-30 07:31:50 +0300

    Move ProductServiceTests to correct folder

--- tests/Northwind.Application.UnitTests/Services/ProductServiceTests.cs
+++ tests/Northwind.Application.UnitTests/Services/ProductServiceTests.cs
@@ -12,7 +12,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
-namespace Northwind.Application.UnitTests
+namespace Northwind.Application.UnitTests.Services
 {
     public class ProductServiceTests
     {
