b29c98a6ec23d7c44ca9b1eb3d5765df32e35fe1
Srul1k <72085557+Srul1k@users.noreply.github.com>
2021-11-30 16:27:18 +0300

    Update IProductService.cs
    * Clear the interface

--- src/Northwind.Application/Interfaces/IProductService.cs
+++ src/Northwind.Application/Interfaces/IProductService.cs
@@ -6,11 +6,11 @@ namespace Northwind.Application.Interfaces
 {
     public interface IProductService
     {
-        Task CreateAsync(ProductEntity productEntityDb);
-        Task UpdateAsync(ProductEntity productEntityDb);
+        Task CreateAsync(ProductEntity productEntity);
+        Task UpdateAsync(ProductEntity productEntity);
         Task<ProductEntity> GetByIdAsync(int id);
         Task<IEnumerable<ProductEntity>> GetMaxAmountAsync(int maxAmountOfProducts);
-        Task DeleteAsync(ProductEntity productEntityDb);
+        Task DeleteAsync(ProductEntity productEntity);
         Task<IEnumerable<SupplierEntity>> GetSuppliers();
         Task<IEnumerable<CategoryEntity>> GetCategories();
     }
