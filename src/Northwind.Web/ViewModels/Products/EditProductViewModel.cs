340e80dcf1220f8f3ddb0d7971a5170224d4dddb
Srul1k <72085557+Srul1k@users.noreply.github.com>
2021-12-01 16:36:39 +0300

    Update EditProductViewModel.cs
    * Inherit from CreateProductViewModel to remove duplicate code

--- src/Northwind.Web/ViewModels/Products/EditProductViewModel.cs
+++ src/Northwind.Web/ViewModels/Products/EditProductViewModel.cs
@@ -1,40 +1,10 @@
-﻿using Northwind.Domain.Entities;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Northwind.Web.ViewModels.Products
 {
-    public class EditProductViewModel
+    public class EditProductViewModel : CreateProductViewModel
     {
         [Key]
         public int ProductID { get; set; }
-
-        [Required]
-        [StringLength(50)]
-        public string ProductName { get; set; }
-
-        [StringLength(20)]
-        public string QuantityPerUnit { get; set; }
-
-        [DataType(DataType.Currency)]
-        [Range(0, 922337203685477.5807)]
-        public decimal UnitPrice { get; set; }
-
-        [Range(0, short.MaxValue, ErrorMessage = "The value must be positive")]
-        public short UnitsInStock { get; set; }
-
-        [Range(0, short.MaxValue, ErrorMessage = "The value must be positive")]
-        public short UnitsOnOrder { get; set; }
-
-        [Range(0, short.MaxValue, ErrorMessage = "The value must be positive")]
-        public short ReorderLevel { get; set; }
-
-        [Required]
-        public bool Discontinued { get; set; }
-
-        public int SupplierID { get; set; }
-        public SupplierEntity SupplierEntity { get; set; }
-
-        public int CategoryID { get; set; }
-        public CategoryEntity CategoryEntity { get; set; }
     }
 }
