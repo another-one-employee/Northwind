f3311941a516ad56ae864264b026840f6d89232e
Srul1k <72085557+Srul1k@users.noreply.github.com>
2021-12-01 17:10:37 +0300

    Update Api/ProductModels validation
    * Move common validation logic into a Abstract class

--- src/Northwind.Web/ViewModels/Api/Products/UpdateProductModelValidator.cs
+++ src/Northwind.Web/ViewModels/Api/Products/UpdateProductModelValidator.cs
@@ -2,38 +2,13 @@
 
 namespace Northwind.Web.ViewModels.Api.Products
 {
-    public class UpdateProductModelValidator : AbstractValidator<UpdateProductModel>
+    public class UpdateProductModelValidator : AbstractProductModelValidator<UpdateProductModel>
     {
         public UpdateProductModelValidator()
         {
             RuleFor(cpm => cpm.ProductID)
                 .NotNull()
                 .GreaterThan(0);
-
-            RuleFor(cpm => cpm.ProductName)
-                .NotNull()
-                .MaximumLength(50);
-
-            RuleFor(cpm => cpm.QuantityPerUnit)
-                .MaximumLength(20);
-
-            RuleFor(cpm => cpm.UnitPrice)
-                .InclusiveBetween(0, (decimal) 922337203685477.5807);
-
-            RuleFor(cpm => cpm.UnitsInStock)
-                .InclusiveBetween((short)0, short.MaxValue)
-                .WithMessage("{PropertyName} must be positive");
-
-            RuleFor(cpm => cpm.UnitsOnOrder)
-                .InclusiveBetween((short)0, short.MaxValue)
-                .WithMessage("{PropertyName} must be positive");
-
-            RuleFor(cpm => cpm.ReorderLevel)
-                .InclusiveBetween((short)0, short.MaxValue)
-                .WithMessage("{PropertyName} must be positive");
-
-            RuleFor(cpm => cpm.Discontinued)
-                .NotNull();
         }
     }
 }
